983623c522d27c795aa1521d576c5928eec1d9c1
SaidBC <razzouksaid139@gmail.com>
2025-11-26 06:53:45 +0100

    fix:build error missing env variables

--- utils/serverEnv.ts
+++ utils/serverEnv.ts
@@ -8,6 +8,8 @@ const serverEnv = z.object({
 });
 
 export default serverEnv.parse({
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 });
